d429e18b1094205e6e9396d14a4b1174662158f1
rkynaa <46591178+rkynaa@users.noreply.github.com>
2019-03-04 21:12:52 +0700

    print.php
    The standard use of 'print' in PHP

---
+++ basics/print.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+Name file: print.php
+Main objective(s):
+    1. To learn how to use 'print' in PHP language
+    2. To understand its characteristics
+*/
+
+// The standard of 'print' is equally the same thing like PHP's 'echo'.
+// For instance, if you want to print a string:
+print "Hello World! <br>";
+
+// if you want to print the value of a variable, in any kind of value:
+$text = "PHP";
+print "Hello world! ".$text." is the best!<br>";
+
+// Now, you can fully understand how to use print in PHP and it's similarities with PHP's 'echo'
+
+?>
