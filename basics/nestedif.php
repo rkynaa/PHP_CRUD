c680fed9e422984de61ae99715c307035c302eb7
rkynaa <46591178+rkynaa@users.noreply.github.com>
2019-03-05 12:02:17 +0700

    nestedif.php (DON'T OPEN!)

---
+++ basics/nestedif.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+Name file: nestedif.php
+Main objective(s):
+    1. To learn how to use nested 'if else' condition in PHP language
+*/
+
+// In this file, you will learn how to use nested 'if else' condition.
+// And it's usage is highly familiar with other languages as well.
+
+// Also, you can make it in any kind of combination, no ammter how much.
+// For example:
+  
+
+?>
